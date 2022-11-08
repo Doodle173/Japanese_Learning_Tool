ed6702e7d5a068063a518de325f998be07cb4dfe
grayd173 <52260791+Doodle173@users.noreply.github.com>
2022-11-08 16:00:18 -0500

    removed code that used jisho.org api

--- SearchTool.cs
+++ SearchTool.cs
@@ -19,25 +19,29 @@ namespace jpLearningToolOcr
             this.client = new WebClient();
         }
 
+        //public SearchResult search(string searchValue)
+        //{
+        //    Stream stream = client.OpenRead($"https://jisho.org/api/v1/search/words?keyword={searchValue}");
+        //    StreamReader reader = new StreamReader(stream);
+        //    string json = reader.ReadToEnd();
+        //    stream.Close();
+        //    reader.Close();
+
+        //    JObject o = JObject.Parse(json);
+
+        //    SearchResult result = new SearchResult();
+
+        //    result.word = (string)o.SelectToken("$.data[0].japanese[0].word");
+        //    result.reading = (string)o.SelectToken("$.data[0].japanese[0].reading");
+        //    result.meanings = o.SelectToken("$.data[0].senses[0].english_definitions");
+        //    result.partOfSpeech = o.SelectToken("$.data[0].senses[0].parts_of_speech");
+        //    result.jlpt = (string)o.SelectToken("$.data[0].jlpt[0]");
+
+        //    return result;
+        //}
+
         public SearchResult search(string searchValue)
         {
-            Stream stream = client.OpenRead($"https://jisho.org/api/v1/search/words?keyword={searchValue}");
-            StreamReader reader = new StreamReader(stream);
-            string json = reader.ReadToEnd();
-            stream.Close();
-            reader.Close();
-
-            JObject o = JObject.Parse(json);
-
-            SearchResult result = new SearchResult();
-
-            result.word = (string)o.SelectToken("$.data[0].japanese[0].word");
-            result.reading = (string)o.SelectToken("$.data[0].japanese[0].reading");
-            result.meanings = o.SelectToken("$.data[0].senses[0].english_definitions");
-            result.partOfSpeech = o.SelectToken("$.data[0].senses[0].parts_of_speech");
-            result.jlpt = (string)o.SelectToken("$.data[0].jlpt[0]");
-
-            return result;
         }
 
 
